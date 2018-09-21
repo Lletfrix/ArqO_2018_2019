aa3394660a30e18ff64bd39570f469e421bc5ab1
lletfrix <lletfrix@gmail.com>
2018-09-21 11:11:19 +0200

    Mips estructuras

---
+++ MIPS_EC/Entrega/RegsMIPS.vhd
@@ -0,0 +1,74 @@
+----------------------------------------------------------------------
+-- Fichero: RegsMIPS.vhd
+-- Descripción: Registro para MIPS 
+
+-- Autores: Rafael Sánchez Sánchez y Sergio Galán Martín
+-- Asignatura: EC 1º grado
+-- Grupo de Prácticas: 2101
+-- Grupo de Teoría: 210
+-- Práctica: 3
+-- Ejercicio: 2
+----------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+use IEEE.std_logic_arith.all;
+
+entity RegsMIPS is
+	port (
+		Clk : in std_logic; -- Reloj
+		NRst : in std_logic; -- Reset asíncrono a nivel bajo
+		Wd3 : in std_logic_vector(31 downto 0); -- Dato de escritura
+		We3 : in std_logic; -- Write enable
+		A1 : in std_logic_vector(4 downto 0); -- Dirección Registro Lectura 1
+		A2 : in std_logic_vector(4 downto 0); -- Dirección Registro Lectura 2
+		A3 : in std_logic_vector(4 downto 0); -- Dirección Registro Escritura
+		Rd1 : out std_logic_vector(31 downto 0); -- Salida 1
+		Rd2 : out std_logic_vector(31 downto 0) --Salida 2
+	); 
+end RegsMIPS;
+
+architecture Practica of RegsMIPS is
+
+	-- Tipo para almacenar los registros
+	type regs_t is array (0 to 31) of std_logic_vector(31 downto 0);
+	
+	-- Esta es la señal que contiene los registros. El acceso es de la
+	-- siguiente manera: regs(i) acceso al registro i, donde i es
+	-- un entero. Para convertir del tipo std_logic_vector a entero se
+	-- hace de la siguiente manera: conv_integer(slv), donde
+	-- slv es un elemento de tipo std_logic_vector
+
+	-- Registros inicializados a '0' 
+	-- NOTA: no cambie el nombre de esta señal.
+	signal regs : regs_t;
+begin  -- PRACTICA
+
+	------------------------------------------------------
+	            -- Escritura --
+	------------------------------------------------------
+	process (NRst, Clk)	
+	begin
+		if NRst = '0' then -- Si el Reset está activado (activo a nivel bajo)
+			for i in 0 to 31 loop
+				regs(i)<=(others=>'0'); --Todos los registros a 0
+			end loop;
+		
+		elsif rising_edge(Clk) then --Escritura sincrona
+		if (We3 = '1')	then --Si el WriteEnable está activo
+			if (conv_integer(A3)/=0) then --Y la dirección A3 no es el registro 0
+			regs(conv_integer(A3))<=Wd3; --Escribimos el dato de entrada en ese registro
+			end if;
+		end if;
+		end if;
+	end process;
+	------------------------------------------------------
+	           -- Lectura --
+	------------------------------------------------------
+	Rd1 <= regs(conv_integer(A1)); --Lectura asíncrona del registro indicado por A1
+	Rd2 <= regs(conv_integer(A2)); --Lectura asincrona del registro indicado por A2
+
+end Practica;
+
---
+++ MIPS_EC/UnidadControl.vhd
@@ -0,0 +1,92 @@
+----------------------------------------------------------------------
+-- Fichero: UnidadControl.vhd
+-- Descripción: Unidad de Control para el procesador
+-- Fecha última modificación:
+
+-- Autores: Rafael Sánchez Sánchez y Sergio Galán Martin
+-- Asignatura: EC 1º grado
+-- Grupo de Prácticas: 2101
+-- Grupo de Teoría: 210
+-- Práctica: 5
+-- Ejercicio: 2
+----------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+
+entity UnidadControl is
+  port(
+    OPCode   : in  std_logic_vector (5 downto 0);  -- OPCode de la instrucción
+    Funct    : in  std_logic_vector(5 downto 0);   -- Funct de la instrucción
+    -- Señales para el PC
+    Jump     : out std_logic;
+    Branch   : out std_logic;
+    -- Señales para la memoria
+    MemToReg : out std_logic;
+    MemWrite : out std_logic;
+
+    -- Señales para la ALU
+    ALUSrc     : out std_logic;
+    ALUControl : out std_logic_vector (2 downto 0);
+    ExtCero    : out std_logic;
+
+    -- Señales para el GPR
+    RegWrite : out std_logic;
+    RegDest  : out std_logic
+    );
+end UnidadControl;
+architecture behavioural of UnidadControl is
+begin
+
+  MemtoReg <= '1' when OPCODE = "100011" else  --lw
+              '0';
+
+  MemWrite <= '1' when OPCODE = "101011" else  --sw
+              '0';
+
+
+  Branch <= '1' when OPCODE = "000100" else  --beq
+            '0';
+
+  ALUControl <= "000" when OPCODE = "000000" and FUNCT = "100100" else  --and
+                "010" when OPCODE = "000000" and FUNCT = "100000" else  --add
+                "110" when OPCODE = "000000" and FUNCT = "100010" else  --sub
+                "101" when OPCODE = "000000" and FUNCT = "100111" else  --nor
+                "001" when OPCODE = "000000" and FUNCT = "100111" else  --or
+                "---" when OPCODE = "000000" and FUNCT = "100110" else  --xor
+                "111" when OPCODE = "000000" and FUNCT = "101010" else  --slt
+                "010" when OPCODE = "100011"                      else  --lw
+                "010" when OPCODE = "101011"                      else  --sw
+                "110" when OPCODE = "000100"                      else  --beq
+                "000" when OPCODE = "001100"                      else  --andi
+                "001" when OPCODE = "001101"                      else  --ori
+                "010" when OPCODE = "001000"                      else  --addi
+                "111" when OPCODE = "001010"                      else  --slti
+                "---" when OPCODE = "000010"                      else  --j
+                "000";
+
+  ALUSrc <= '0' when OPCODE = "000000" else  --R-TYPE
+            '0' when OPCODE = "000100" else  --beq
+            '1';
+
+
+  RegDest <= '0' when FUNCT = "000000" else   -- nop
+             '1' when OPCODE = "000000" else  --R-TYPE
+             '0';
+
+  RegWrite <= '0' when OPCODE = "101011" else  --sw
+              '0' when OPCODE = "000100" else  --beq
+              '0' when OPCODE = "000010" else  --j
+              '0' when OPCODE = "000000" else  -- nop
+              '1';
+
+  ExtCero <= '1' when OPCODE = "001100" or OPCODE = "001101" else  --andi y ori
+             '0';
+
+
+  Jump <= '1' when OPCODE = "000010" else  --j
+          '0';
+
+end behavioural;
