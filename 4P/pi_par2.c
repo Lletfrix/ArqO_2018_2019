3022dd14b3ca2cba9bdb5ba74a26018e49563c88
lletfrix <lletfrix@gmail.com>
2018-12-12 22:29:08 +0100

    final commit

--- 4P/pi_par2.c
+++ 4P/pi_par2.c
@@ -1,5 +1,5 @@
 /*********************************************************/
-/* Cálculo PI: Método de integración - Version OpenMP	 */
+/* Cï¿½lculo PI: Mï¿½todo de integraciï¿½n - Version OpenMP	 */
 /* false sharing 					 */
 /*********************************************************/
 
@@ -9,7 +9,7 @@
 #include <unistd.h>
 #include <omp.h>
 
-int main( int argc, char *argv[] ) 
+int main( int argc, char *argv[] )
 {
 	int i, n = 100000000, numProcs, numThreads;
 	double h, pi, *sum = NULL,t = 0;
@@ -19,9 +19,9 @@ int main( int argc, char *argv[] )
 	numProcs = omp_get_num_procs();
 	printf("Numero de cores del equipo: %d\n", numProcs);
 	omp_set_num_threads( numProcs );
-	
+
 	// Solicitamos memoria para la suma de cada proceso
-	sum = (double*) malloc (sizeof(double)*numProcs);	
+	sum = (double*) malloc (sizeof(double)*numProcs);
 
 	gettimeofday(&t1,NULL);
 
@@ -30,28 +30,28 @@ int main( int argc, char *argv[] )
 	{
 		double x;
 		int i, tid;
-	
-		// Obtenemos el numero de procesos lanzados	
+
+		// Obtenemos el numero de procesos lanzados
                 numThreads = omp_get_num_threads();
-	
+
 		// Obtenemos nuestro identificador de thread
 		tid = omp_get_thread_num();
-	
+
 		sum[tid] = 0.0;
-		for(i = tid + 1; i <= n; i += numThreads) 
+		for(i = tid + 1; i <= n; i += numThreads)
 		{
 			x = h * ((double) i - 0.5);
 			sum[tid] += 4.0 / (1.0 + x*x);
 		}
 	}
-	
+
 	pi = 0.0;
 	for(i = 0; i < numProcs; i++)
 		pi += sum[i];
 	pi = h * pi;
-	
+
 	gettimeofday(&t2,NULL);
-	
+
 	t = (t2.tv_sec -t1.tv_sec) + (t2.tv_usec - t1.tv_usec)/(double)1000000;
 	printf("Resultado pi: %f\nTiempo %lf\n", pi, t);
 
